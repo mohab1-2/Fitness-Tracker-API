5a3eaee21451646e31d0b6b4a9b1f9f7395f060f
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-09-04 23:13:25 +0300

    Update wsgi.py

--- Fitness Tracker project/FitnessTracker/FitnessTracker/wsgi.py
+++ Fitness Tracker project/FitnessTracker/FitnessTracker/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for FitnessTracker project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FitnessTracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FitnessTracker.FitnessTracker.settings')
 
 application = get_wsgi_application()
